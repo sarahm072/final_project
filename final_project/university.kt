2b48934577f48e76fe901426681a64b6f81b091e
Sara Meraouche <148572793+sarahm072@users.noreply.github.com>
2024-06-13 21:09:48 +0100

    University.kt

--- final_project/university.kt
+++ final_project/university.kt
@@ -1,15 +1,17 @@
 package final_project
 
-class university (var name :String, val address :String, val email :String,val phoneNumber :String) {
-    val listOfFaculties :ArrayList<String> = ArrayList()
+class University (var name :String, val address :String, val email :String, val phoneNumber :String) {
+    val listOfFaculties :ArrayList<Faculty> = ArrayList()
     var directorOfTheUniv :String=""
-    fun CreateUniversity():university{
-        return this
+    fun CreateUniversity(name :String, address :String,email :String,phoneNumber :String):University{
+        val  univ = University(name,address,email,phoneNumber)
+        return univ
     }
-    fun AddFaculty(Faculty:String){
+
+    fun AddFaculty(Faculty:Faculty){
         listOfFaculties.add(Faculty)
     }
-    fun RemoveFaculty(Faculty:String){
+    fun RemoveFaculty(Faculty:Faculty){
        listOfFaculties.remove(Faculty)
     }
 }
