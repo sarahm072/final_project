63b71bb918332b3ac55317d5084f820aa01b7fbd
Sara Meraouche <148572793+sarahm072@users.noreply.github.com>
2024-07-06 15:55:59 +0100

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.app
+
+import android.graphics.drawable.shapes.RoundRectShape
+import android.inputmethodservice.Keyboard.Row
+import android.media.Image
+import android.os.Bundle
+import android.provider.FontsContract
+import android.text.Layout
+import android.text.Layout.Alignment
+import android.text.Layout.Alignment.ALIGN_NORMAL
+import android.view.RoundedCorner
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.*
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.app.ui.theme.AppTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.material3.MaterialTheme
+import androidx.core.provider.FontsContractCompat.Columns
+import org.intellij.lang.annotations.JdkConstants.BoxLayoutAxis
+import androidx.compose.material3.Surface
+//Image
+import com.example.app.R
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.draw.clip
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    App(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App( modifier: Modifier = Modifier) {
+        Surface(color = Color.DarkGray) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = androidx.compose.ui.Alignment.Center){
+            val imagePainter: Painter = painterResource(R.drawable.dark)
+
+            Image(
+                painter = imagePainter,
+                contentDescription = "Description for accessibility",
+//                modifier = Modifier.clip(shape = RoundedCircleShape())
+            )
+         }
+        }
+    }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    AppTheme {
+        App()
+    }
+}
